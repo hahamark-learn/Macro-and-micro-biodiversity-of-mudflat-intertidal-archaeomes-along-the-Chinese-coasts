5ca8288ff2ca80bee83dc3ac3955545b07c1dc18
Yanli-EvaCode <52338657+hahamark-learn@users.noreply.github.com>
2025-05-16 17:08:30 +0800

    Update map.R

--- Fig1/map.R
+++ Fig1/map.R
@@ -1,31 +1,30 @@
+#Load the directory where the file is located
 setwd("~/Desktop/article1/third_version/")
-# 安装和加载必要的包
+#Install necessary packages (if not already installed)
 install.packages("ggplot2")
 install.packages("maps")
 install.packages("cowplot")
 install.packages("rnaturalearth")
 install.packages("ggspatial")
-#加载相关的包
+#Load the packages
 library(ggplot2)
 library(maps)
 library(cowplot)
 library(rnaturalearth)
 library(sf)
 library(ggspatial)
-#在线读入数据，读入全世界数据
+#Read data online, load global data
 tiny_countries <- ne_countries(type = "sovereignty", scale = 10)
-# 获取中国省份数据
+#Retrieve data for Chinese provinces
 china_provinces <- ne_states(country = "China", returnclass = "sf")
-# 获取全球海洋数据
+#Retrieve global ocean data
 oceans <- ne_download(scale = 10, type = "ocean", category = "physical", returnclass = "sf")
-#全世界河流数据
+#Global river data
 rivers <- ne_download(scale = 10, type = 'rivers_lake_centerlines', category = 'physical', returnclass = "sf")
-#十断线数据读取
-ceshi2<- "十段线.shp"
+#Ten-dash line data retrieval
+ceshi2<- "the-ten-dasg-line.shp"
 ten_line<- st_read(ceshi2)
-
-
-
+#Generate the overall plot
 map_all <- ggplot() +
   geom_sf(data = tiny_countries,color= "#AA9F93",fill="#FBF9F5") +
   geom_sf(data = china_provinces, color = "#44AFDA",fill="#E6F2EE",linewidth = 0.03)+
@@ -36,14 +35,14 @@ map_all <- ggplot() +
   geom_sf(data=ten_line,color= "black")+
   coord_sf(xlim = c(70, 136), ylim = c(14, 55)) +
   annotation_north_arrow(
-    location = "tr",  # 指北针位置：右上角（top-right）
-    style = north_arrow_fancy_orienteering(),  # 指北针样式
-    pad_x = unit(0.1, "cm"),  # 水平方向的内边距
+    location = "tr",  # Compass position: top-right
+    style = north_arrow_fancy_orienteering(),  # Compass style
+    pad_x = unit(0.1, "cm"),  # Horizontal padding
     pad_y = unit(0.1, "cm"),
     width = unit(1, "cm"),height = unit(1, "cm")
   )+
   theme_minimal() 
-
+#Plot the ten-dash line data
 map1 <-ggplot() +
   geom_sf(data = tiny_countries,color= "#AA9F93",fill="#FBF9F5") +
   geom_sf(data = china_provinces, color = "#44AFDA",fill="#E6F2EE",linewidth = 0.03)+
@@ -54,15 +53,12 @@ map1 <-ggplot() +
   geom_sf(data=ten_line,color= "black")+
   coord_sf(xlim = c(105, 120), ylim = c(0,25 )) +
   theme_minimal() 
-
+#Merge the map of the ten-dash line with the overall map
 final_map <- ggdraw()+
   draw_plot(map_all)+
   draw_plot(map1,x=0.7,y=0.15,width = 0.3,height = 0.3)
-
-
-
+#Show the pattern
 print(final_map)
-
-
-ggsave("china_provinces_map_13.pdf", width = 8.27, height = 11.69)
+#Save the PDF file"
+ggsave("china_provinces_map.pdf", width = 8.27, height = 11.69)
 
