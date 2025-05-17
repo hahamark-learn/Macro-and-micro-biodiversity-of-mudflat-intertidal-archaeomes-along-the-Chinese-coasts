ff65be7c57866db32f0faee0c468832a1bc72481
Yanli-EvaCode <52338657+hahamark-learn@users.noreply.github.com>
2025-05-17 15:38:50 +0800

    Update ldg_ddr.R

--- Fig5/ldg_ddr.R
+++ Fig5/ldg_ddr.R
@@ -1,5 +1,7 @@
-#kegg_contig_绘图
-#加载对应的包
+‘’‘
+Figure 5 plotting information, some of the data can be obtained from the data sources in the article
+’‘’
+#Load the corresponding package
 library(tidyverse)
 library(reshape2)
 library(tibble)
@@ -8,7 +10,8 @@ library(dplyr)
 library(vegan)
 library(ggpmisc)
 library(RColorBrewer)
-setwd("/Users/hahamark/Desktop/article1/宏基因组分析/contig_taxnonmy/coverm_count/kegg/")
+#Load the corresponding folder
+setwd("~/coverm_count/kegg/")
 files <- list.files(pattern = "kegg_result")
 combined_data <- data.frame()
 for (i in files){
@@ -22,43 +25,39 @@ for (i in files){
   }
   
 }
-
-
-
 combined_data[is.na(combined_data)] <- 0
 write.table(file = "all_kegg_count_reads.txt",combined_data,quote = F,sep = "\t")
-#通过coverm mean 来排出异常值#k00059
+#Filter out outliers using the coverm mean
 kegg_data_integer<- read.table("all_kegg_count_reads_clean.txt",header = T,row.names = 1,check.names = F)
 
-#进行抽平在进行分析
+#Perform normalization before conducting the analysis
 colSums(kegg_data_integer)
-#使用该代码抽平
 kegg_data_Flattening = as.data.frame(t(rrarefy(t(kegg_data_integer), min(colSums(kegg_data_integer)))))
 rownames(kegg_data_Flattening) <- row.names(kegg_data_integer)
 tkegg_data_Flattening <- t(kegg_data_Flattening)
-write.table(file = "/Users/hahamark/Desktop/article1/宏基因组分析/contig_taxnonmy/coverm_count/kegg/all_contig_kegg_raw_flattening.txt",kegg_data_Flattening,quote = F,sep = "\t")
-#进行抽平在进行分析
+write.table(file = "all_contig_kegg_raw_flattening.txt",kegg_data_Flattening,quote = F,sep = "\t")
+#Calculate richness and merge the data with the corresponding locations
 richness_kegg <- specnumber(kegg_data_Flattening,MARGIN = 2)
 richness_kegg<- data.frame(richness_kegg)
 colnames(richness_kegg) <- "richness"
-location_kegg <- read.table('~/Desktop/article1/宏基因组分析/基于read宏基因组注释/location-ldg.txt',header = T,row.names = 1,check.names = F)
+location_kegg <- read.table('location-ldg.txt',header = T,row.names = 1,check.names = F)
 data_kegg_richness_location <- merge(richness_kegg,location_kegg,by = "row.names")
 colnames(data_kegg_richness_location)[3] <-c("Latitude")
 colnames(data_kegg_richness_location)[4] <-c("location")
 data_kegg_richness_location$type <- "function"
-#读入16s相关数据已经抽平了：
-data_16s <- read.table("~/Desktop/article1/16s分析/16s-all/ASVs_counts_clean_Flattening-rdp-new.txt",header = T,row.names = 1,check.names = F)
+#Load the 16S richness-related data and merge it with the corresponding location data
+data_16s <- read.table("ASVs_counts_clean_Flattening-rdp-new.txt",header = T,row.names = 1,check.names = F)
 richness_16s <- data.frame(specnumber(data_16s,MARGIN = 2))
 tdata_16s_Flattening <- t(data_16s)
 colnames(richness_16s) <- "richness"
-location_16s <- read.table("~/Desktop/article1/16s分析/16s-all/location-ldg.txt",header = T,row.names = 1)
+location_16s <- read.table("location-ldg-16s.txt",header = T,row.names = 1)
 data_16s_richness_location <- merge(richness_16s,location_16s,by="row.names")
 colnames(data_16s_richness_location)[3] <-c("Latitude")
 colnames(data_16s_richness_location)[4] <-c("location")
 data_16s_richness_location$type <- "taxonomy"
-#整合数据合并16s和read数据
+#Integrate the data by merging the 16S and read data.
 data_all_richness_location <- rbind(data_16s_richness_location,data_kegg_richness_location)
-#赋予颜色
+#Assign colors
 color1 <- c("#1f77b4","#f6b5d1")
 p1 <- ggplot(data_all_richness_location,aes(Latitude,richness,color=type))+
   geom_point()+
@@ -69,26 +68,26 @@ p1 <- ggplot(data_all_richness_location,aes(Latitude,richness,color=type))+
     parse = T
   )+theme_bw()+
   scale_color_manual(values = color1)
+ggsave("16s_kegg_ldg.pdf",p1,width = 8,height = 6)
 
-ggsave("/Users/hahamark/Desktop/article1/宏基因组分析/contig_taxnonmy/coverm_count/kegg/16s_kegg_ldg_2025.5.9.pdf",p1,width = 8,height = 6)
-#计算ddr
+#Calculate the DDR
 bray_distance_tasv <- vegdist(tkegg_data_Flattening,method = "bray")
 data1 <- as.matrix(bray_distance_tasv,row.names=1)
 data1[upper.tri(data1)]=NA
 data2 <- melt(data1,na.rm = T)
-write.table(data2,"/Users/hahamark/Desktop/article1/宏基因组分析/contig_taxnonmy/coverm_count/kegg/kegg_flattening_bray_similirty.txt",quote = FALSE, row.names = FALSE,sep = "\t")
-#经过for_ddr，python脚本处理后读入
-data_kegg_ddr <- read.table("/Users/hahamark/Desktop/article1/宏基因组分析/contig_taxnonmy/coverm_count/kegg/kegg_flattening_bray_similirty_get_location.txt",header = T)
+write.table(data2,"kegg_flattening_bray_similirty.txt",quote = FALSE, row.names = FALSE,sep = "\t")
+#Load the data after processing with the for_ddr Python script.
+data_kegg_ddr <- read.table("kegg_flattening_bray_similirty_get_location.txt",header = T)
 
-#计算16s_ddr的情况
+#Calculate the 16S DDR
 bray_distance_tasv <- vegdist(tdata_16s_Flattening,method = "bray")
 data1 <- as.matrix(bray_distance_tasv,row.names=1)
 data1[upper.tri(data1)]=NA
 data2 <- melt(data1,na.rm = T)
-write.table(data2,"~/Desktop/article1/宏基因组分析/contig_taxnonmy/cd_hit_result/kegg/16s_flattening_bray_similirty.txt",quote = FALSE, row.names = FALSE,sep = "\t")
-#经过for_ddr，python脚本处理后读入
-data_16s_ddr <- read.table("~/Desktop/article1/宏基因组分析/contig_taxnonmy/cd_hit_result/kegg/16s_flattening_bray_similirty_get_location.txt",header = T)
-#合并数据
+write.table(data2,"16s_flattening_bray_similirty.txt",quote = FALSE, row.names = FALSE,sep = "\t")
+#Load the data after processing with the for_ddr Python script for 16S DDR calculation
+data_16s_ddr <- read.table("16s_flattening_bray_similirty_get_location.txt",header = T)
+#Merge the data
 data_16s_ddr$type <- "taxonomy"
 data_kegg_ddr$type <- "function"
 data_all_ddr <- rbind(data_16s_ddr,data_kegg_ddr)
@@ -101,37 +100,4 @@ p2 <- ggplot(data_all_ddr,aes(x=log10(distance),y=log10(similarity),color=type))
   theme_bw()+
   scale_color_manual(values = color1)
 p2
-ggsave("/Users/hahamark/Desktop/article1/宏基因组分析/contig_taxnonmy/coverm_count/kegg/16s_kegg_ddr_2025.5.9.pdf",p2,width = 8,height = 6)
-
-
-#pcoa 
-kegg_data_Flattening <- read.table("/Users/hahamark/Desktop/article1/宏基因组分析/contig_taxnonmy/coverm_count/kegg/all_contig_kegg_raw_flattening.txt",header = T,row.names = 1,check.names = F)
-
-color1 <- brewer.pal(12,"Set3")
-tdata1 <- t(kegg_data_Flattening)
-distance <- vegdist(tdata1,method = "bray")
-pcoa <- cmdscale(distance,k=(nrow(tdata1)-1),eig = T)
-#提取前两个分类进行解释
-plot_data <- data.frame({pcoa$point})[1:2]
-#前两个分类解释命名
-names(plot_data)[1:2] <- c('PCoA1','PCoA2')
-eig=pcoa$eig
-plot_data$id <- rownames(plot_data)
-data <- merge(plot_data,location_kegg,by = "row.names")
-data$Location <- factor(data$Location,levels= c("Sanya","Beihai","Zhuhai","Ningbo","Yancheng",
-                                                   "Lianyungang","Shantou","Xiamen","Wenzhou",
-                                                   "Qingdao","Dongying","Dandong"))
-#基于bray-curtis距离进行计算
-asv1.div <- adonis2(tdata1 ~ Location,data=location_kegg,permutations = 999,method = "bray")
-#把统计结果放在pcoa上
-asv1_adonis <- paste0("adonis R2: ",round(asv1.div$R2,2)," P-value:",asv1.div$`Pr(>F)`)
-
-p3<- ggplot(data,aes(x=data$PCoA1,y=data$PCoA2,color=Location))+
-  geom_point(alpha=1,size=3)+
-  stat_ellipse(level = 0.95,size=1)+
-  scale_color_manual(values = color1)+
-  labs(x=paste("PCoA1(",format(100*eig[1]/sum(eig),digits = 4),"%)",sep=""),y=paste("PCoA2(",format(100*eig[2]/sum(eig),digits = 4),"%)",sep=""),title = asv1_adonis)+
-  geom_vline(aes(xintercept=0),linetype="dotted")+geom_hline(aes(yintercept=0),linetype="dotted")+
-  theme(panel.background = element_rect(fill='white',colour = 'black'),axis.title.x=element_text(colour = 'black',size=20),axis.title.y = element_text(colour = 'black',size=20),legend.text = element_text(size = 15))
-p3
-ggsave("/Users/hahamark/Desktop/article1/宏基因组分析/contig_taxnonmy/coverm_count/kegg/pcoa-kegg-25.5.9.pdf",p3,width = 8,height = 6)
+ggsave("16s_kegg_ddr.pdf",p2,width = 8,height = 6)
