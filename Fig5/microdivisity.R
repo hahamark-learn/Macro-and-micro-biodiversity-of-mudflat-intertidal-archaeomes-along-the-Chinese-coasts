a036c63c1892fce86de6e38f9051004b3c4f4c95
Yanli-EvaCode <52338657+hahamark-learn@users.noreply.github.com>
2025-05-17 15:43:09 +0800

    Update microdivisity.R

--- Fig5/microdivisity.R
+++ Fig5/microdivisity.R
@@ -1,16 +1,20 @@
+'''
+The correlation between microdiversity and macrodiversity.
+'''
+#Load the corresponding package
 library(ggplot2)
 library(RColorBrewer)
 library(vegan)
 library(ggpubr)
 library(ggpmisc)
-#微观多样性
-setwd("~/Desktop/article1/宏基因组分析/基于kraken_contig微观多样性/10.Microdiversity/")
+#Load the corresponding folder 
+setwd("~Microdiversity/")
 data1 <- read.table('metapop-pi-average-1.txt',header = T,sep = "\t")
 data1$group <-factor(data1$group,levels=c("Sanya", "Beihai" ,"Zhuhai","Shantou","Xiamen","Wenzhou","Ningbo","Yancheng","Lianyungang",
                                           "Qingdao","Dongying","Dandong"))
 color1 <- brewer.pal(12,"Set3")
 
-#箱线图
+#Plot a boxplot
 p2 <- ggplot(data1,aes(group,value,fill=group))+
   geom_boxplot()+
   scale_fill_manual(values = color1)+
@@ -18,25 +22,11 @@ p2 <- ggplot(data1,aes(group,value,fill=group))+
   theme(axis.text.x = element_text(angle = 45))+ylab("Nucleotide diversity")+
   xlab("Location")
 
-ggsave("micro_2025.5.9.pdf",p2,width = 8,height = 6)
+ggsave("micro_2025.pdf",p2,width = 8,height = 6)
 
-#ldg
-
-ggplot(data1,aes(x=Latitude,y=value))+
-  geom_point(aes(colour=data1$group))+
-  geom_smooth(method = "lm",formula =y ~ poly(x, 2),color="black")+
-  scale_color_manual(values = color1)+ggprism::theme_prism()+
-  ylab("value of π")+
-  stat_cor()
-  
-
-#  stat_poly_eq(aes(label = paste(..eq.label.., ..p.value.label.., sep = '~~~~')),label.y = "bottom", label.x = "left")
-
-
-#读入richness 相关数据
+#Load the richness-related data
 data2<- read.table("read_richness",header = T,row.names = 1)
 data2$ID <-row.names(data2)
-
 data3 <- merge(data1,data2,by="ID")
 p1<-ggplot(data3,aes(x=Richness11,y=value))+
   geom_point()+
@@ -51,23 +41,3 @@ p1<-ggplot(data3,aes(x=Richness11,y=value))+
   )
 p1
 ggsave("relationship_macro_micro_2025.5.9.pdf",p1,width = 8,height = 6)
-
-
-#读入16s数据
-
-
-data4 <- read.table("16-richness",header = T,row.names = 1)
-
-data3 <- merge(data1,data4,by.x = "ID",by.y="row.names")
-
-ggplot(data3,aes(x=richness,y=value))+
-  geom_point()+
-  geom_smooth(method = "lm",formula =y ~ poly(x),color="black")+
-  ggprism::theme_prism()+
-  ylab("value of pi")+
-  xlab("value of richness")+
-  stat_poly_eq(
-    aes(label = paste(..eq.label.., ..rr.label.., ..p.value.label.., sep = "~~~")),
-    formula = y ~ x, 
-    parse = T
-  )
