2c10a7e87e8d4ef254065359e4cc3f1085ada401
Yanli-EvaCode <52338657+hahamark-learn@users.noreply.github.com>
2025-05-17 15:53:44 +0800

    Update snp-plot.R

--- Fig5/snp-plot.R
+++ Fig5/snp-plot.R
@@ -1,9 +1,12 @@
-setwd("Desktop/article1/宏基因组分析/基于kraken_contig微观多样性/10.Microdiversity/")
+‘’‘
+Plot data for a single SNP
+’‘’
+setwd("~/Microdiversity/")
 data1 <- read.table("snp-ploting",header = T,sep = "\t")
 library(trackViewer)
 library(ggplot2)
 SNP <- data1$pos
-yaxis <- c(0,5,10,15,20,25,30,35,40,45,50,55,60)
+yaxis <- c(0,5,10,15,20,25,30,35,40,45,50,55,60)#The numbers here represent the differences between different locations
 xaxis <- c(1,400,800,1200,1400,1573)
 
 sample.gr <- GRanges("chr1", IRanges(SNP, width=1, names=paste0("snp", SNP)))
@@ -17,16 +20,3 @@ features$height <- c(0.05)
 lolliplot(sample.gr, features,yaxis = yaxis,xaxis = xaxis,jitter=c("node", "label"),legendPosition = 'right',lollipop_style_switch_limit=5)
 
 ggsave(file="snp-ceshi6.pdf",p1,width = 20,height = 10)
-
-
-lolliplot(sample.gr, features,jitter="label")
-
-data1<-sample.int(15, length(sample.gr), replace=TRUE)
-
-
-SNP <- c(10,11,100, 105, 108, 400, 410, 420, 600, 700, 805, 840, 1400, 1402)w
-sample.gr <- GRanges("chr1", IRanges(SNP, width=1, names=paste0("snp", SNP)))
-lolliplot(sample.gr, features)
-
-c("#FF8833", "#F9712A", "#DFA32D", 
-  "#51C6E6", "#009DDA", "#4B9CDF")
